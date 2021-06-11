375ad4c1fc633c1944bfc80aa27c8eefc0c14e85
slx618 <slx618@hotmail.com>
2021-06-11 11:53:39 +0800

    day 01

--- src/github.com/slx618/day01/01helloworld/main.go
+++ src/github.com/slx618/day01/01helloworld/main.go
@@ -7,6 +7,10 @@ import "fmt"
 //函数外只能放置标识符(变量 常量 函数 类型)的声明
 
 //CGO_ENABLED=0 GOOS=linux GOARCH=amd64
+//CGO_ENABLED=0 GOOS=darwin GOARCH=amd64
+//CGO_ENABLED=0 GOOS=windows GOARCH=amd64
+//go build -o xxx 输出文件
+//go install 先编译再移动文件
 func main() {
 	fmt.Println("hello world")
 }
--- src/github.com/slx618/day01/02var/main.go
+++ src/github.com/slx618/day01/02var/main.go
@@ -1,4 +1,4 @@
-package main
+package main //包名
 
 import "fmt"
 
