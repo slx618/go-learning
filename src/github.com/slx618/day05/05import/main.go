027163f1b7a7f2034bcfa554583035843a156885
slx618 <slx618@hotmail.com>
2021-06-21 10:26:24 +0800

    day 05

--- src/github.com/slx618/day05/05import/main.go
+++ src/github.com/slx618/day05/05import/main.go
@@ -6,7 +6,7 @@ import (
 	//. fmt 这么引入 fmt.Println 可以简写 Println 但是不建议用
 
 	//别名 & 路径是从 src 开始的
-	calcx "github.com/slx618/day05/06calc"
+	calc "github.com/slx618/day05/06calc"
 	//不允许导入包但是不使用包, 因为 go 是编译型的 会增加包冗余
 	//匿名导入会自动执行 init 只执行 init
 	//_ "src"
@@ -18,6 +18,6 @@ func init() {
 }
 
 func main() {
-	calcx.Add(2, 3)
+	calc.Add(2, 3)
 	fmt.Println("sss")
 }
